d5ecf730e4234257ee320bfbf7c0896f3d5ecb77
karmazinvaceslav <vyacheslavkarmazin@gmail.com>
2021-01-12 12:35:04 +0100

    initialization project

---
+++ src/main/java/com/world/entertainment/worldentertainment/WorldEntertainmentApplication.java
@@ -0,0 +1,13 @@
+package com.world.entertainment.worldentertainment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldEntertainmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldEntertainmentApplication.class, args);
+    }
+
+}
