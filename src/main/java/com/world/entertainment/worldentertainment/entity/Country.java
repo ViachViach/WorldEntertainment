8aa3bbf2283fcb09366812869846f5dcf8bf0346
Boris Savrasov <b.savrasov@devcaz.com>
2021-02-24 20:22:48 +0100

    added country entity

---
+++ src/main/java/com/world/entertainment/worldentertainment/entity/Country.java
@@ -0,0 +1,87 @@
+package com.world.entertainment.worldentertainment.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Table(schema = "public")
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToMany(mappedBy = "entertainment")
+    private Set<Entertainment> entertainments;
+
+    private String name;
+    private String code;
+
+    private Date createAt;
+    private Date updateAt;
+    private Date deleteAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public Country setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Country setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public Country setCode(String code) {
+        this.code = code;
+        return this;
+    }
+
+    public Set<Entertainment> getEntertainments() {
+        return entertainments;
+    }
+
+    public Country setEntertainments(Set<Entertainment> entertainments) {
+        this.entertainments = entertainments;
+        return this;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public Country setCreateAt(Date createAt) {
+        this.createAt = createAt;
+        return this;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public Country setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+        return this;
+    }
+
+    public Date getDeleteAt() {
+        return deleteAt;
+    }
+
+    public Country setDeleteAt(Date deleteAt) {
+        this.deleteAt = deleteAt;
+        return this;
+    }
+}
